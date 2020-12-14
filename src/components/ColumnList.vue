0c19fea00ee5569a26a8aca92a9163f3efb5f08b
wuliaojj <923084543@qq.com>
2020-12-14 23:15:26 +0800

    初步完成ColumnList组件

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,36 +1,27 @@
 <template>
-  <ul>
-    <li v-for="column in list" :key="column.id">
-      <img :src="column.avatar" :alt="column.title">
-      <h5>{{column.title}}</h5>
-      <p>{{column.description}}</p>
-      <a href="#">进入专栏</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img  :src="column.avatar" :alt="column.title" class="rounded-circle border border-light w-25 my-3" >
+          <h5 class="card-title">{{column.title}}</h5>
+          <p class="card-text text-left">{{column.description}}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, computed } from 'vue'
+
 export interface ColumnProps {
   id: number;
   title: string;
-  avatar: string;
+  avatar?: string;
   description: string;
 }
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
 export default defineComponent({
   name: 'ColumnList',
   props: {
@@ -38,6 +29,19 @@ export default defineComponent({
       // 这里特别有一点，我们现在的 Array 是没有类型的，只是一个数组，我们希望它是一个 ColomnProps 的数组，那么我们是否可以使用了类型断言直接写成 ColomnProps[]，显然是不行的 ，因为 Array 是一个数组的构造函数不是类型，我们可以使用 PropType 这个方法，它接受一个泛型，讲 Array 构造函数返回传入的泛型类型。
       type: Array as PropType<ColumnProps[]>,
       required: true
+    }
+  },
+  setup (props) {
+    const columnList = computed(() => {
+      return props.list.map(column => {
+        if (!column.avatar) {
+          column.avatar = require('../assets/logo.jpg')
+        }
+        return column
+      })
+    })
+    return {
+      columnList
     }
   }
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
